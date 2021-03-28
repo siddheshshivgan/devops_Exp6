b98fe28c70cefbd177301c1d5a68b2f9a353a284
Siddhesh Shivgan <siddheshshivgan2306@gmail.com>
2021-03-28 12:17:28 +0530

    inital commit

--- job1.java
+++
@@ -1,6 +0,0 @@
-class job1
-{
-	public static void main(String[] args){
-		System.out.println("Job1 Done!");
-	}
-}
---
+++ job2.java
@@ -0,0 +1,6 @@
+class job2
+{
+	public static void main(String[] args){
+		System.out.println("Job2 Done!");
+	}
+}
